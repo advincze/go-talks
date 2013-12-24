41adb935336663bcb7f8c8f5e58f8c38a119f6f3
Adam <avincze@Adams-MacBook-Pro-2.local>
2013-12-24 07:26:35 +0100

    cleaned up unused files

--- hello.go
+++ hello.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "fmt"
 
-func bla() {
-	fmt.Println("Hello world!")
-	println("blubber")
-	log.Println("kaslkdfj")
-	var b bool
-	if b {
-		println("blubber2")
-	}
+func main() {
+	fmt.Println("Hello, go")
 }
